87e029f1df72fdd353b4260b010b2a10743fb92d
DineshBSGit <dinesh.kumar@banksathi.com>
2024-06-03 20:20:24 +0530

    adding module

--- app/src/main/java/com/learn/blinkeffectexample/MainActivity.kt
+++ app/src/main/java/com/learn/blinkeffectexample/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.learn.blinkeffectexample
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val click :TextView  = findViewById(R.id.textProducts)
+        click.setOnClickListener({
+//            val intent = Intent(this, Ban::class.java)
+//            intent.putExtra("LEAD_ID", leadDetailData.value?.leadId)
+//            startActivity(intent)
+        })
     }
 }
